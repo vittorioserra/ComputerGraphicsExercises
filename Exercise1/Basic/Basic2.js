8a5444fe78202c628b6935415134fd6dd452d5f4
elsapepagarcia <lidiafar25@gmail.com>
2022-11-03 20:35:53 +0100

    Update Basic2.js

--- Exercise1/Basic/Basic2.js
+++ Exercise1/Basic/Basic2.js
@@ -2,11 +2,16 @@
 
 function drawArcCircle(canvas) {
     let context = canvas.getContext("2d");
-
-    //TODO 1.2)       Use the arc() function to
-    //                rasterize the two circles
-    //                from Task 1.1.
-
-
+    let img = context.createImageData(200, 200);
+    context.fillStyle = 'rgba(0, 255, 0)';
+    context.strokeStyle = 'rgba(0, 127, 0)';
+    context.lineWidth = 20;
+    context.beginPath();
+    context.arc(140, 140, 45, 0, 2 * Math.PI);
+    context.fill()
+    context.stroke();
+    context.moveTo(60+50, 60);
+    context.arc(60, 60, 50, 0, 2 * Math.PI);
+    context.fill();
 
 }
