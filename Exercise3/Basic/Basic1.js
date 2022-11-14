653c2019900d039f46f1c48f4dce9a566c383977
elsapepagarcia <lidiafar25@gmail.com>
2022-11-14 14:52:08 +0100

    Update Basic1.js
    Basic1 done!

--- Exercise3/Basic/Basic1.js
+++ Exercise3/Basic/Basic1.js
@@ -14,23 +14,40 @@ function webGLStart(canvas) {
     let vertices = [];
     let indices = [];
 
+    "use strict";
+
+function webGLStart(canvas) {
+
+    let gl = canvas.getContext("experimental-webgl");
+    if (!gl) throw new Error("Could not initialise WebGL, sorry :-(\nTo enable WebGL support in your browser, go to about:config and skip the warning.\nSearch for webgl.disabled and set its value to false.");
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+    let c = [0, 0];
+    let r = 0.7;
+    let slices = 100;
+    
+    let vertices = [];
+    let indices = [];
+
     // TODO 3.1)	Replace the following code so that
     //              the vertices and indices to not describe
     //              a triangle but a circle around the center
     //              c with radius r. Use triangles to describe 
     //              the circle's geometry. The number of
     //              triangles is stored in the variable slices.
-
-    vertices.push(-.5);
-    vertices.push(-.5);
-    vertices.push(.5);
-    vertices.push(-.5);
-    vertices.push(0);
-    vertices.push(0.5);
-
-    indices.push(0);
-    indices.push(1);
-    indices.push(2);
+    vertices.push(c[0])
+    vertices.push(c[1])
+    for(var i=0; i<slices; i++){
+        
+        vertices.push((r*Math.cos((2*Math.PI*i)/slices))+c[0]);
+        vertices.push((r*Math.sin((2*Math.PI*i)/slices))+c[1]);
+        vertices.push((r*Math.cos((2*Math.PI*(i+1))/slices))+c[0]);
+        vertices.push((r*Math.sin((2*Math.PI*(i+1))/slices))+c[1]);
+        indices.push(0);
+        indices.push(2*i+1);
+        indices.push(2*(i+1));
+    }
 
     
 
