1b8f1685085481a6966d01d0d47800a9c83e0097
Vittorio Serra <vittorio.serra96@gmail.com>
2022-11-15 15:16:46 +0000

    first basic exercise done

--- Exercise3/Basic/Basic1.js
+++ Exercise3/Basic/Basic1.js
@@ -10,46 +10,51 @@ function webGLStart(canvas) {
     let c = [0.3, 0.2];
     let r = 0.7;
     let slices = 100;
-    
-    let vertices = [];
-    let indices = [];
 
-    "use strict";
-
-function webGLStart(canvas) {
-
-    let gl = canvas.getContext("experimental-webgl");
-    if (!gl) throw new Error("Could not initialise WebGL, sorry :-(\nTo enable WebGL support in your browser, go to about:config and skip the warning.\nSearch for webgl.disabled and set its value to false.");
-
-    gl.viewport(0, 0, canvas.width, canvas.height);
-
-    let c = [0, 0];
-    let r = 0.7;
-    let slices = 100;
-    
     let vertices = [];
     let indices = [];
 
     // TODO 3.1)	Replace the following code so that
     //              the vertices and indices to not describe
     //              a triangle but a circle around the center
-    //              c with radius r. Use triangles to describe 
+    //              c with radius r. Use triangles to describe
     //              the circle's geometry. The number of
     //              triangles is stored in the variable slices.
-    vertices.push(c[0])
-    vertices.push(c[1])
-    for(var i=0; i<slices; i++){
-        
-        vertices.push((r*Math.cos((2*Math.PI*i)/slices))+c[0]);
-        vertices.push((r*Math.sin((2*Math.PI*i)/slices))+c[1]);
-        vertices.push((r*Math.cos((2*Math.PI*(i+1))/slices))+c[0]);
-        vertices.push((r*Math.sin((2*Math.PI*(i+1))/slices))+c[1]);
+
+
+    //base_vertex = center ;
+    c = [0,0]; // let's put this in the middle for now
+    let vertex_left_x = 0.7;
+    let vertex_left_y = 0.0;
+    let vertex_right_x = 0.0;
+    let vertex_right_y = 1.0;
+
+    vertices.push(0);//push center point
+    vertices.push(0);
+
+    vertices.push(vertex_left_x);
+    vertices.push(vertex_left_y);
+
+    for(let i = 2; i < slices; i++){
+        vertex_right_x = r*Math.cos(2*Math.PI*i/slices);
+        vertex_right_y = r*Math.sin(2*Math.PI*i/slices);
+
+        vertices.push(vertex_right_x);
+        vertices.push(vertex_right_y);
+
         indices.push(0);
-        indices.push(2*i+1);
-        indices.push(2*(i+1));
+        indices.push(i-1);
+        indices.push(i);
+
+        console.log("Current point is x : %f , y : %f \n", vertex_right_x, vertex_right_y);
+
     }
 
-    
+    //stitch first and last vertices together
+    indices.push(0);
+    indices.push(1);
+    indices.push(slices-1);
+
 
     let vbo = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
