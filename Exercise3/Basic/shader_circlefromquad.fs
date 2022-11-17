0a1956e5a43dff8ce9073994d9130001a40e3152
elsapepagarcia <lidiafar25@gmail.com>
2022-11-17 12:12:47 +0100

    Update shader_circlefromquad.fs

--- Exercise3/Basic/shader_circlefromquad.fs
+++ Exercise3/Basic/shader_circlefromquad.fs
@@ -2,7 +2,7 @@ precision mediump float;
 
 // TODO 3.3)	Define a constant variable (uniform) to 
 //              "send" the canvas size to all fragments.
-
+uniform vec2 canvasSize;
 void main(void)
 { 
 	float smoothMargin = 0.01;  
@@ -13,5 +13,22 @@ void main(void)
 	//				with the radius r. Smooth the circle's edge within 
 	//				[r-smoothMargin, r] by computing an appropriate alpha value.
 
-	gl_FragColor = vec4(1.0, 85.0 / 255.0, 0.0, 1.0);
+	highp vec2 uv = vec2(gl_FragCoord.xy);
+
+	uv[0] = (uv[0]/(canvasSize[0]/2.0))-1.0;
+	uv[1] = (uv[1]/(canvasSize[1]/2.0))-1.0;
+
+
+	if((length(uv))>r){
+		discard;
+	}else if(length(uv)>(r-smoothMargin)){
+		float alpha = 0.0 + ((r) - length(uv))*1.0;
+
+		gl_FragColor = vec4(1.0, 85.0 / 255.0, 0.0, alpha);
+
+	} else{
+
+		gl_FragColor = vec4(1.0, 85.0 / 255.0, 0.0, 1.0);
+	}
+
 }
